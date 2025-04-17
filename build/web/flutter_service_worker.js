08d4f66718a992575be604b5a6f177dab5c2fba2
MontasirOpi <montasiropi@gmail.com>
2025-04-17 10:47:03 +0600

    error fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "628b69a6abdeeed6238267184dd286e7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f74faf791833bb20d61678fa15500f3d",
+"flutter_bootstrap.js": "49637ef8536a6160dad436eabb9450a3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
