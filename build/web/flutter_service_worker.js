f595d1e211a5b201ee34deb97ef04f4d6554f0a1
MontasirOpi <montasiropi@gmail.com>
2025-06-30 14:55:46 +0600

    feat: resolve resume download issue and build for web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7aa5fa325ca7cfa342b57bbddde797c5
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "332fb3311486d58b39371cc40d87a4f0",
+"flutter_bootstrap.js": "abf69c7125bb7db2861d093a63c32303",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
